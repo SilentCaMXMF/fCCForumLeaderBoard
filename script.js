f52fc6e46ca2bc9a089dd705009824eb70791b84
SilentCaMXMF <pedroocalado@gmail.com>
2025-03-13 00:59:42 +0000

    Finished / Working project

--- script.js
+++ script.js
@@ -5,24 +5,84 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
+
+    selectedCategory.className = className;
+    selectedCategory.category = category;
+  } else {
+    selectedCategory.className = "general";
+    selectedCategory.category = "General";
+    selectedCategory.id = 1;
+  }
+  const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+  const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
+
+  return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}
+  </a>`;
+};
+
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = new Date(time);
-  const diffInMs = currentTime - lastPost; // Difference in milliseconds
-  const diffInMinutes = Math.floor(diffInMs / (1000 * 60)); // Convert to minutes
 
-  if (diffInMinutes < 60) {
-    return `${diffInMinutes}m ago`;
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
   }
 
-  const diffInHours = Math.floor(diffInMinutes / 60); // Convert to hours
-  if (diffInHours < 24) {
-    return `${diffInHours}h ago`;
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
   }
 
-  const diffInDays = Math.floor(diffInHours / 24); // Convert to days
-  return `${diffInDays}d ago`;
-  
+  return `${daysAgo}d ago`;
+};
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
+};
+
+const avatars = (posters, users) => {
+  return posters
+    .map((poster) => {
+      const user = users.find((user) => user.id === poster.user_id);
+      if (user) {
+        const avatar = user.avatar_template.replace(/{size}/, 30);
+        const userAvatarUrl = avatar.startsWith("/user_avatar/")
+          ? avatarUrl.concat(avatar)
+          : avatar;
+        return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+      }
+    })
+    .join("");
 };
 
 const fetchData = async () => {
@@ -56,12 +116,18 @@ const showLatestPosts = (data) => {
     return `
     <tr>
       <td>
-        <p class="post-title">${title}</p>
+        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
+
+        ${forumCategory(category_id)}
       </td>
-      <td></td>
+      <td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
+      </td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
-      <td></td>
+      <td>${viewCount(views)}</td>
+      <td>${timeAgo(bumped_at)}</td>
     </tr>`;
   }).join("");
 };
